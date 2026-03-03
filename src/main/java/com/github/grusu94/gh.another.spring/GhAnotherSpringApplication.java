6009fda711bc8fd1168b999a7d2bb104e0ef4b26
grusu94 <georgerusu994@gmail.com>
2026-03-03 07:31:27 +0200

    Initial commit

---
+++ src/main/java/com/github/grusu94/gh.another.spring/GhAnotherSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.grusu94.gh.another.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhAnotherSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhAnotherSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/grusu94/gh/another/spring/GhAnotherSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.grusu94.gh.another.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhAnotherSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
